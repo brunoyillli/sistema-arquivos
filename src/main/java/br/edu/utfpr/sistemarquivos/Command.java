63b08a19a9394bf0388d17cc55e082cdc9ed1e3f
Bruno Mendes <wolfteam1910@gmail.com>
2022-10-17 08:53:02 -0300

    subindo projeto

---
+++ src/main/java/br/edu/utfpr/sistemarquivos/Command.java
@@ -0,0 +1,152 @@
+package br.edu.utfpr.sistemarquivos;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public enum Command {
+
+    LIST() {
+        @Override
+        boolean accept(String command) {
+            final var commands = command.split(" ");
+            return commands.length > 0 && commands[0].startsWith("LIST") || commands[0].startsWith("list");
+        }
+
+        @Override
+        Path execute(Path path) throws IOException {
+
+            // TODO implementar conforme enunciado
+
+            return path;
+        }
+    },
+    SHOW() {
+        private String[] parameters = new String[]{};
+
+        @Override
+        void setParameters(String[] parameters) {
+            this.parameters = parameters;
+        }
+
+        @Override
+        boolean accept(String command) {
+            final var commands = command.split(" ");
+            return commands.length > 0 && commands[0].startsWith("SHOW") || commands[0].startsWith("show");
+        }
+
+        @Override
+        Path execute(Path path) {
+
+            // TODO implementar conforme enunciado
+
+            return path;
+        }
+    },
+    BACK() {
+        @Override
+        boolean accept(String command) {
+            final var commands = command.split(" ");
+            return commands.length > 0 && commands[0].startsWith("BACK") || commands[0].startsWith("back");
+        }
+
+        @Override
+        Path execute(Path path) {
+            if (path.compareTo(Paths.get(Application.ROOT)) == 0) {
+                return path;
+            }
+            return path.getParent();
+        }
+    },
+    OPEN() {
+        private String[] parameters = new String[]{};
+
+        @Override
+        void setParameters(String[] parameters) {
+            this.parameters = parameters;
+        }
+
+        @Override
+        boolean accept(String command) {
+            final var commands = command.split(" ");
+            return commands.length > 0 && commands[0].startsWith("OPEN") || commands[0].startsWith("open");
+        }
+
+        @Override
+        Path execute(Path path) {
+
+            // TODO implementar conforme enunciado
+
+            return path;
+        }
+    },
+    DETAIL() {
+        private String[] parameters = new String[]{};
+
+        @Override
+        void setParameters(String[] parameters) {
+            this.parameters = parameters;
+        }
+
+        @Override
+        boolean accept(String command) {
+            final var commands = command.split(" ");
+            return commands.length > 0 && commands[0].startsWith("DETAIL") || commands[0].startsWith("detail");
+        }
+
+        @Override
+        Path execute(Path path) {
+
+            // TODO implementar conforme enunciado
+
+            return path;
+        }
+    },
+    EXIT() {
+        @Override
+        boolean accept(String command) {
+            final var commands = command.split(" ");
+            return commands.length > 0 && commands[0].startsWith("EXIT") || commands[0].startsWith("exit");
+        }
+
+        @Override
+        Path execute(Path path) {
+            System.out.print("Saindo...");
+            return path;
+        }
+
+        @Override
+        boolean shouldStop() {
+            return true;
+        }
+    };
+
+    abstract Path execute(Path path) throws IOException;
+
+    abstract boolean accept(String command);
+
+    void setParameters(String[] parameters) {
+    }
+
+    boolean shouldStop() {
+        return false;
+    }
+
+    public static Command parseCommand(String commandToParse) {
+
+        if (commandToParse.isBlank()) {
+            throw new UnsupportedOperationException("Type something...");
+        }
+
+        final var possibleCommands = values();
+
+        for (Command possibleCommand : possibleCommands) {
+            if (possibleCommand.accept(commandToParse)) {
+                possibleCommand.setParameters(commandToParse.split(" "));
+                return possibleCommand;
+            }
+        }
+
+        throw new UnsupportedOperationException("Can't parse command [%s]".formatted(commandToParse));
+    }
+}
---
+++ src/main/java/br/edu/utfpr/sistemarquivos/FileReader.java
@@ -0,0 +1,10 @@
+package br.edu.utfpr.sistemarquivos;
+
+import java.nio.file.Path;
+
+public class FileReader {
+
+    public void read(Path path) {
+        // TODO implementar a leitura dos arquivos do PATH aqui
+    }
+}
